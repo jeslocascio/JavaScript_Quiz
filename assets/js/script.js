c953b48b2ae8bc926b6b616b0aeebcd1a9483b14
jeslocascio <134552933+jeslocascio@users.noreply.github.com>
2023-07-18 23:49:47 -0400

    Added functional Start Button to webpage. Added CSS to make page look nicer (will need to add Global Variables). Made start button change question text. Attempting to declare a 'wrong answer', but not working yet.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,19 @@
 var timerEl = document.getElementById('countdown');
-var mainEl = document.getElementById('main');
+var start = document.getElementById('start');
+var answer = document.getElementsByClassName('option_button');
+var timeLeft = 80;
+var testQuestions = document.getElementsByClassName('response_box');
+testQuestions.style = document.getElementsByClassName('response_box');
+var theQuestion = document.getElementById('questionText');
+var buttonOne = document.getElementById('option_1');
+var buttonTwo = document.getElementById('option_2');
+var buttonThree = document.getElementById('option_3');
+var buttonFour = document.getElementById('option_4');
+var wrongButton = document.getElementsByClassName('option_button');
+var answerCheck = document.getElementById('iscorrect');
+
 
 function countdown() {
-  var timeLeft = 80;
-
   var timeInterval = setInterval(function () {
     if (timeLeft > 0) {
       timerEl.textContent = 'Time: ' + timeLeft;
@@ -15,7 +25,25 @@ function countdown() {
   }, 1000);
 }
 
+start.addEventListener('click', function() {
+  if(start) {
+    countdown();
+    start.style="display:none";
+    testQuestions.style="display:block";
+    theQuestion.textContent = 'What does the DOM stand for?';
+    buttonOne.textContent = 'Directive Override Mission';
+    buttonTwo.textContent = 'Document Object Model';
+    buttonThree.textContent = 'Discovery Objective Model';
+    buttonFour.textContent = 'Dismount Obstacles Mission';
+  }
+})
 
-countdown();
+wrongButton.addEventListener('click', function() {
+  if(wrongButton) {
+    answerCheck.textContent = 'Wrong Answer!';
+  } else {
+    answerCheck.textContent = 'Correct!';
+  }
+})
 
 
