4a635b537a8e1accbd7b9eca2aeed22ca49fd3b6
jeslocascio <134552933+jeslocascio@users.noreply.github.com>
2023-07-21 23:48:32 -0400

    In the middle of trial-and-error trying to get working quiz. Adjusted css and HTML accordingly. Not done by any means, just stopping for the night.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,23 @@
 var timerEl = document.getElementById('countdown');
 var start = document.getElementById('start');
-var answer = document.getElementsByClassName('option_button');
+// var answer = document.getElementsByClassName('option_button');
 var timeLeft = 80;
-var testQuestions = document.getElementsByClassName('response_box');
-testQuestions.style = document.getElementsByClassName('response_box');
-var theQuestion = document.getElementById('questionText');
-var buttonOne = document.getElementById('option_1');
-var buttonTwo = document.getElementById('option_2');
-var buttonThree = document.getElementById('option_3');
-var buttonFour = document.getElementById('option_4');
-var wrongButton = document.getElementsByClassName('option_button');
-var answerCheck = document.getElementById('iscorrect');
-
+// var testQuestions = document.querySelector('.response_box');
+// // testQuestions.style = document.getElementsByClassName('response_box');
+// var theQuestion = document.getElementById('questionText');
+// var buttonOne = document.getElementById('option_1');
+// var buttonTwo = document.getElementById('option_2');
+// var buttonThree = document.getElementById('option_3');
+// var buttonFour = document.getElementById('option_4');
+// var button = document.getElementsByClassName('option_button');
+// var answerCheck = document.getElementById('iscorrect');
+// var correctAnswer = buttonTwo;
+// var wrongAnswer = buttonOne, buttonThree, buttonFour;
+var questionEl = document.getElementById("question");
+var answerEl = document.getElementById("answer-buttons");
+var currentQuestionIndex = 0;
+var score = timeLeft;
+var testing = document.getElementById("theChoice");
 
 function countdown() {
   var timeInterval = setInterval(function () {
@@ -25,25 +31,123 @@ function countdown() {
   }, 1000);
 }
 
+
+
 start.addEventListener('click', function() {
   if(start) {
     countdown();
     start.style="display:none";
-    testQuestions.style="display:block";
-    theQuestion.textContent = 'What does the DOM stand for?';
-    buttonOne.textContent = 'Directive Override Mission';
-    buttonTwo.textContent = 'Document Object Model';
-    buttonThree.textContent = 'Discovery Objective Model';
-    buttonFour.textContent = 'Dismount Obstacles Mission';
+    currentQuestionIndex = 0;
+    score = timeLeft;
+    showQuestion();
   }
 })
 
-wrongButton.addEventListener('click', function() {
-  if(wrongButton) {
-    answerCheck.textContent = 'Wrong Answer!';
+
+var questions = [
+  {
+    question: "What does the DOM stand for?",
+    answers: [
+      {text: 'Directive Override Mission', correct: false},
+      {text: 'Document Object Model', correct: true},
+      {text: 'Discovery Objective Model', correct: false},
+      {text: 'Dismount Obstacles Mission', correct: false},
+    ]
+  },
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    answers: [
+      {text: '<script>', correct: true},
+      {text: '<link>', correct: false},
+      {text: '<div>', correct: false},
+      {text: '<header>', correct: false},
+    ]
+  },
+ {
+    question: "Where do you link the JavaScript in the HTML file?",
+    answers: [
+      {text: '<head>', correct: false},
+      {text: '<div>', correct: false},
+      {text: '<body>', correct: true},
+      {text: '<header>', correct: false},
+    ]
+  },
+  {
+    question: "How do you create a function in JavaScript?",
+    answers: [
+      {text: 'myFunction function()', correct: false},
+      {text: 'function myFunction{}', correct: false},
+      {text: 'function = myFunction()', correct: false},
+      {text: 'function myFunction()', correct: true},
+    ]
+  }
+]
+
+function showQuestion(){
+  resetState();
+  var currentQuestion = questions[currentQuestionIndex];
+  var questionNo = currentQuestionIndex + 1;
+  questionEl.innerHTML = questionNo + "." + currentQuestion.question;
+
+  currentQuestion.answers.forEach(answer => {
+    var button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.setAttribute("id", "answer_choice")
+    button.classList.add("btn_answer");
+    answerEl.appendChild(button);
+    if(answer.correct){
+      testing.style="display:block";
+    }
+    button.addEventListener("click", selectAnswer);
+  });
+}
+
+function resetState(){
+  start.style.display = "none";
+  while (answerEl.firstChild){
+    answerEl.removeChild(answerEl.firstChild);
+  } 
+};
+
+function selectAnswer(e){
+  var selectedBtn = e.target;
+  var isCorrect = selectedBtn.dataset.correct === "true";
+  if(isCorrect){
+    selectedBtn.classList.add("correct");
+    testing.textContent = "Correct";
+    nextQuestion();
   } else {
-    answerCheck.textContent = 'Correct!';
+    selectedBtn.classList.add("incorrect");
+    testing.textContent = "Incorrect";
+    timeLeft = timeLeft - 10;
+    nextQuestion();
   }
-})
+  Array.from(answerEl.children).forEach(button => {
+    if(button.dataset.correct === true){
+      button.classList.add("correct");
+    }
+    button.disabled = true;
+   
+  })
+}
+
+function nextQuestion() {
+  if(currentQuestionIndex < questions.length){
+    currentQuestionIndex++;
+    showQuestion();
+  } else {
+    submitScore();
+  }
+}
+
+function submitScore() {
+  console.log("You did it!");
+}
 
 
+
+
+// "What does the DOM stand for?" 'Document Object Model',
+// Inside which HTML element do we put the JavaScript? '<script>'
+// Where is the correct place to insert a JavaScript? 'The <body> section
+// How do you create a function in JavaScript? 'function myFunction()'
